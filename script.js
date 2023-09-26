ccec2983c08b5f4f94ada6e35396d5db6dde1366
OlgaPetrova17 <petrova1783@gmail.com>
2023-09-26 15:17:38 +0200

    Ts39 Классы конструкторы 1

--- script.js
+++ script.js
@@ -5,13 +5,29 @@ class Phone {
         this.model = phoneModel;
         this.weight = phoneWeight;
     }
+    receiveCall(name) {
+        console.log(`Звонит ${name} на телефоне ${this.model}. Вес ${this.weight}`);
+    }
 }
-let catalogPhones = [];
-// let myPhone: Phone = new Phone('+375291309753', 'xiaomi11', 120) //объект класса
-// console.log(`вес: ${myPhone.weight}  модель: ${myPhone.model} телефон: ${myPhone.number}`)
-// const model: Phone = new Phone()
-// const weight: Phone = new Phone()
-// console.log(number)
-// console.log(model)
-// console.log(weight)
+let catalogPhones = new Array(10); //создание массива определённой длины
+let olgaPhone = new Phone('1111', 'xiaomi', 119);
+catalogPhones[0] = olgaPhone;
+for (let index = 0; index < catalogPhones.length; index++) {
+    catalogPhones[index] = new Phone('1111', 'xiaomi', 119);
+    catalogPhones[index].weight = catalogPhones[index].weight + index;
+}
+let sergeyPhone = catalogPhones[4];
+sergeyPhone.model = `samsung`;
+for (let index = 0; index < catalogPhones.length; index++) {
+    console.log(catalogPhones[index], `индекс массива = ${index}`);
+}
+olgaPhone.receiveCall('Olga');
+let namesOfCalling = [`Алексей`, `Сергей`, `Ира`, `Петра`, `Роберт`, `Аги`, `Лена`, `Оля Б.`, `Сергей Ф.`, `Паша`];
+console.log(namesOfCalling);
+// for (let index = 0; index < catalogPhones.length; index++) {    //задача 5
+//     catalogPhones[index].receiveCall(namesOfCalling[index])
+// }
+catalogPhones.forEach(element => {
+});
+//forEach стрелочная функция 2 параметра элемент массива и 2ой параметр индекс
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -8,18 +8,59 @@ class Phone {
         this.model = phoneModel;
         this.weight = phoneWeight
     }
+
+    receiveCall(name: string) {
+        console.log(`Звонит ${name} на телефоне ${this.model}. Вес ${this.weight}`)
+    }
 }
 
-let catalogPhones: Phone[] = []
-let olga: Phone = new Phone('1111', 'xiaomi', 119)
-catalogPhones[1] = olga
+let catalogPhones: Phone[] = new Array(10) //создание массива определённой длины
 
-// let myPhone: Phone = new Phone('+375291309753', 'xiaomi11', 120) //объект класса
-// console.log(`вес: ${myPhone.weight}  модель: ${myPhone.model} телефон: ${myPhone.number}`)
+let olgaPhone: Phone = new Phone('1111', 'xiaomi', 119)
+catalogPhones[0] = olgaPhone
 
-// const model: Phone = new Phone()
-// const weight: Phone = new Phone()
+for (let index = 0; index < catalogPhones.length; index++) {
+    catalogPhones[index] = new Phone('1111', 'xiaomi', 119);
+    catalogPhones[index].weight = catalogPhones[index].weight + index
+}
 
-// console.log(number)
-// console.log(model)
-// console.log(weight)
+let sergeyPhone = catalogPhones[4];
+sergeyPhone.model = `samsung`
+
+for (let index = 0; index < catalogPhones.length; index++) {
+    console.log(catalogPhones[index], `индекс массива = ${index}`);
+}
+
+olgaPhone.receiveCall('Olga')
+
+let namesOfCalling: string[] = [`Алексей`, `Сергей`, `Ира`, `Петра`, `Роберт`, `Аги`, `Лена`, `Оля Б.`, `Сергей Ф.`, `Паша`]
+console.log(namesOfCalling)
+
+
+// for (let index = 0; index < catalogPhones.length; index++) {    //задача 5, решение правильное
+//     catalogPhones[index].receiveCall(namesOfCalling[index])
+// }
+
+// catalogPhones.forEach((catalogPhones[index], index: number) => {                         1.ВАРИАНТ
+//     catalogPhones[index].receiveCall(namesOfCalling[index]
+// });
+
+// receiveCall.forEach((catalogPhones[index], index: number) => {                         //2.ВАРИАНТ
+//         catalogPhones[index].receiveCall(namesOfCalling[index]
+//     });
+
+//forEach стрелочная функция 2 параметра элемент массива и 2ой параметр индекс
+
+// scores.forEach((value, ind) => {
+    // console.log(`Работаем с ${ind} элементом массива. Его значение = ${value}`)
+//     if (value > max) {   //здесь считать значение max
+//         max = value
+//         overMax++
+//         console.log(`  Число превышений максимума = ${overMax}`)
+//     }
+//     if (value < min) {
+//         min = value
+//         overMin++
+//         console.log(`  Число превышений минимума = ${overMin}`)
+//     }
+// })
